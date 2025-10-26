682ac70ae2cac301557b526047c816504027cf19
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2025-10-25 22:23:35 -0300

    updates db setup script

--- db-init.sql
+++ db-init.sql
@@ -1,27 +1,27 @@
 CREATE TABLE IF NOT EXISTS courses
 (
-    id VARCHAR(25) NOT NULL,
+    id VARCHAR(8) NOT NULL,
     title VARCHAR(50) NOT NULL,
     description VARCHAR(200) NOT NULL,
     img VARCHAR(60),
-    link VARCHAR(80) NOT NULL,
-    tags VARCHAR(100) NOT NULL,
+    link VARCHAR(200) NOT NULL,
+    tags VARCHAR(200) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS projects
 (
-    id VARCHAR(25) NOT NULL,
+    id VARCHAR(8) NOT NULL,
     name VARCHAR(50) NOT NULL,
     description VARCHAR(200) NOT NULL,
     img VARCHAR(60),
-    tags VARCHAR(100) NOT NULL,
+    tags VARCHAR(200) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS secrets_place
 (
-    id VARCHAR(25) NOT NULL,
+    id VARCHAR(8) NOT NULL,
     login VARCHAR(50) NOT NULL,
     password VARCHAR(60) NOT NULL,
     PRIMARY KEY (id)

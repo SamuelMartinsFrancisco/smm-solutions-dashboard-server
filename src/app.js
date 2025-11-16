8ee892dff387f68154c857072d5fff168dd7fd33
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2025-11-15 22:36:08 -0300

    improves cors settings treatment

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 import express from 'express';
-import cors from 'cors';
 import coursesRouter from './routes/courses.routes.js';
 
-const app = express()
 
-app.use(cors());
+function corsSettings (req, res, next) {
+  var allowedDomains = ['https://smm-solutions-dashboard.onrender.com','https://smm-solutions-website.onrender.com' ];
+  var origin = req.headers.origin;
+  if(allowedDomains.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  switch (origin) {
+    case allowedDomains[0]:
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      break;
+    default:
+      res.setHeader('Access-Control-Allow-Methods', 'GET');    
+  }
+
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+}
+
+const app = express()
+app.use(corsSettings);
 app.use(express.json());
 app.use('/api', coursesRouter);
 

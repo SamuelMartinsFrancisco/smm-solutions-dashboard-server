628f025f2a55b553636b9c9f805331e6386e3cf1
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2025-11-15 21:18:53 -0300

    adds fallback server listen port

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 
 dotenv.config();
 
-app.listen(process.env.SERVER_PORT, () => {
+app.listen(process.env.SERVER_PORT || 5000, () => {
     console.log(`\n The server is listening on port ${process.env.SERVER_PORT}! 😁 \n`)
 })
